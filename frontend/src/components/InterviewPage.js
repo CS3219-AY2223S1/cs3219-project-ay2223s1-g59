4e07329bae2397d5e6af29e5c20041d2b7433e1d
ongweisheng <ongweisheng30@gmail.com>
2022-10-05 22:27:00 +0800

    Fix end interview bug

--- frontend/src/components/InterviewPage.js
+++ frontend/src/components/InterviewPage.js
@@ -31,15 +31,29 @@ const InterviewPage = () => {
         setShowEndInterview(true)
     }
     const handleEndInterview = () => {
-        MatchingService.deleteInterview(interviewId)
-            .then((res) => {
-                setQuestionTitle("")
-                setQuestionDescription("")
-                setShowEndInterview(false)
-                navigate("/home")
-            })
-            .catch((err) => {
-                console.log(err)
+        MatchingService
+            .getInterview(interviewId)
+            .then(interviewDetails => {
+                console.log(interviewDetails)
+                if (interviewDetails.data === null) {
+                    console.log("Interview terminated from other user")
+                    setQuestionTitle("")
+                    setQuestionDescription("")
+                    setShowEndInterview(false)
+                    navigate("/home")
+                } else {
+                    MatchingService
+                        .deleteInterview(interviewId)
+                        .then((res) => {
+                            setQuestionTitle("")
+                            setQuestionDescription("")
+                            setShowEndInterview(false)
+                            navigate("/home")
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
+                }
             })
     }
 
