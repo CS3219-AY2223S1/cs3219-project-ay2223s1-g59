7c1299e1aaceb26e7efd09abca96e9e03af9a89d
Herrekt <herrickkohyukan@gmail.com>
2022-10-05 17:11:26 +0800

    Fixed Code Change Issue and added comments
    Tested code change via postman.
    Fixed issue with socket emitting code change and receiving code change.
    Added comments indicating purpose and also highlighting issue not addressed.

--- collaboration-service/index.js
+++ collaboration-service/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cors());
 app.options("*", cors());
 
-// routes tested via postman
+// Routes tested via Postman
 app.use("/collab", collaborationRoutes);
 
 const httpServer = createServer(app);
@@ -37,23 +37,23 @@ httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-//socket-work. Tested using https://socketio.bloggernepal.com/4.0.1/
+// Sockets tested using https://socketio.bloggernepal.com/4.0.1/
+// Also tested via Postman
 const io = new Server(httpServer, {
   cors: {
     origin: true,
   },
 });
 
-// When a client connects
+// When a client initiate socket connection.
 io.on("connection", (socket) => {
-  //test simple connection
-  console.log("a user connected");
+  console.log("A user connected");
 
   socket.on("disconnect", () => {
     console.log("a user disconnected");
   });
 
-  //client joins a specified room
+  // Client joins a specific room.
   socket.on("CONNECTED", async ({ roomId, username }) => {
     console.log(`Connecting to room ${roomId}`);
     const { users } = await Room.findOne({ roomId: roomId });
@@ -70,16 +70,17 @@ io.on("connection", (socket) => {
     console.log(`Connected to room ${roomId}`);
   });
 
-  // when the code is changed.
+  // Code change occurs
   socket.on("CHANGE", async ({ roomId, code }) => {
     console.log("Editing cocde");
     console.log(code);
     socket.broadcast
       .to(`${roomId}`)
-      .emit("CHANGE", { roomId: roomId, code: code });
+      .emit("RECEIVE", { roomId: roomId, code: code });
+    // Sends an event indicating to the other user in the room to update the code they're on.
   });
 
-  //client leaves a room
+  // Client leaves room.
   socket.on("DISCONNECTED", async ({ roomId, username }) => {
     console.log(`Disconnecting from room ${roomId}`);
     const room = await Room.findOne({ roomId: roomId });
@@ -94,8 +95,8 @@ io.on("connection", (socket) => {
     } else {
       await Room.findOneAndDelete({ roomId: roomId });
     }
-    //we don't create rooms we attach socket connections to room.
-    //io.in(roomId).emit("ROOM:CONNECTION", newUsers);
+    // We don't create rooms we attach socket connections to room.
+    // Might want to edit it to work with the normal disconnection if that occurs accidentally in the future.
     socket.leave(`${roomId}`);
     console.log(`Disconnected from room ${roomId}`);
   });
--- collaboration-service/model/room-model.js
+++ collaboration-service/model/room-model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-//import { v4 as uuidv4 } from "uuid";
+//import { v4 as uuidv4 } from "uuid"; // Getting roomId via using interview/matching Id.
 
 let Schema = mongoose.Schema;
 
