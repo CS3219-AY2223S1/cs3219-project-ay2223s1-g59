fe6a78bd2542673703e1c937566227a8bb726cbc
ongweisheng <ongweisheng30@gmail.com>
2022-10-08 00:37:14 +0800

    Add APIs for history service

---
+++ history-service/controllers/historyController.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose"
+import History from "../models/history.js"
+
+export const createHistory = async (req, res) => {
+    try {
+        const history = await History.create(req.body)
+        return res.status(200).json({ message: "interview saved to history "})
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+export const getHistory = async (req, res) => {
+    try {
+        const username = req.params.username
+
+        const histories = await History.find({ username: username })
+        return res.status(200).json(histories)
+    } catch (err) {
+        return res.status(500).json(err)
+    }
+}
