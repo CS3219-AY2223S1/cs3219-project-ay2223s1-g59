2f53b8ceccaa51bbabf7bd75df8b372aa91c5617
ongweisheng <ongweisheng30@gmail.com>
2022-10-08 13:53:08 +0800

    Set up skeleton code for history page

---
+++ frontend/src/components/HistoryPage.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import NavBar from './NavBar'
+import {
+    Container,
+    Stack, 
+    Button,
+    ToggleButton,
+    Form,
+    Table
+} from 'react-bootstrap'
+import HistoryService from "../services/historyService.js"
+
+const HistoryPage = () => {
+    return (
+        <>
+            History Page
+        </>
+    )
+}
