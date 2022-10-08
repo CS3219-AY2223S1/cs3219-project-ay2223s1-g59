a19af3963817fc728ff091d20380cbaff0cf2e30
Herrekt <herrickkohyukan@gmail.com>
2022-10-08 12:49:52 +0800

    Remove commented out areas
    Removed commented out redundant areas (index.js)

--- collaboration-service/index.js
+++ collaboration-service/index.js
@@ -1,34 +1,14 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
-// import mongoose from "mongoose";
 import "dotenv/config";
-import { Server, Socket } from "socket.io";
-import Room from "./model/room-model.js";
-import collaborationRoutes from "./routes/collaboration-routes.js";
-
-// const uri =
-//   process.env.ENV == "PROD"
-//     ? process.env.DB_CLOUD_URI
-//     : process.env.DB_LOCAL_URI;
-
-// mongoose
-//   .connect(uri)
-//   .then((x) =>
-//     console.log(
-//       `Connected to MongoDB! Database name: "${x.connections[0].name}"`
-//     )
-//   )
-//   .catch((err) => console.error("Error connecting to MongoDB", err.reason));
+import { Server } from "socket.io";
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.options("*", cors());
-
-// Routes tested via Postman
-// app.use("/collab", collaborationRoutes);
 
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 8003;
@@ -37,7 +17,6 @@ httpServer.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
 
-// Sockets tested using https://socketio.bloggernepal.com/4.0.1/
 // Also tested via Postman
 const io = new Server(httpServer, {
   cors: {
@@ -56,11 +35,6 @@ io.on("connection", (socket) => {
   // Client joins a specific room.
   socket.on("CONNECTED", async ({ roomId }) => {
     console.log(`Connecting to room ${roomId}`);
-    // const { users } = await Room.findOne({ roomId: roomId });
-    // console.log(users);
-    // let newUsers = users;
-    // newUsers.push(username);
-    // await Room.findOneAndUpdate({ roomId: roomId }, { users: newUsers });
     socket.join(`${roomId}`);
     console.log(`Connected to room ${roomId}`);
   });
@@ -76,20 +50,6 @@ io.on("connection", (socket) => {
   // Client leaves room.
   socket.on("DISCONNECTED", async ({ roomId }) => {
     console.log(`Disconnecting from room ${roomId}`);
-    // const room = await Room.findOne({ roomId: roomId });
-    // const users = room.users;
-    // const newUsers = users.filter((user) => user !== username);
-    // if (newUsers.length) {
-    //   await Room.findOneAndUpdate(
-    //     { roomId: roomId },
-    //     { users: newUsers },
-    //     { new: true }
-    //   );
-    // } else {
-    //   await Room.findOneAndDelete({ roomId: roomId });
-    // }
-    // We don't create rooms we attach socket connections to room.
-    // Might want to edit it to work with the normal disconnection if that occurs accidentally in the future.
     socket.leave(`${roomId}`);
     console.log(`Disconnected from room ${roomId}`);
   });
