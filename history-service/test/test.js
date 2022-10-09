985e0419027c156a8455a614fab0c97e17092e5b
ongweisheng <ongweisheng30@gmail.com>
2022-10-09 21:32:33 +0800

    Update test cases

--- history-service/test/test.js
+++ history-service/test/test.js
@@ -6,8 +6,6 @@ import { userHistory } from "./testData.js"
 chai.use(chaiHttp)
 chai.should()
 
-let username
-
 describe("history-service tests", () => {
     describe("POST/create-history", () => {
         it("Create history successful", (done) => {
@@ -17,23 +15,8 @@ describe("history-service tests", () => {
                 .end((err, res) => {
                     res.should.have.status(200)
                     res.body.should.be.a("object")
-                    res.body.should.have.property("username")
-                    assert.equal(res.body.username, "test1")
-                    username = res.body.username
-                    res.body.should.have.property("matchUsername")
-                    assert.equal(res.body.matchUsername, "test2")
-                    res.body.should.have.property("difficulty")
-                    assert.equal(res.body.difficulty, easy)
-                    res.body.should.have.property("question")
-                    res.body.should.have.deep.property("question.difficulty")
-                    assert.equal(res.body.question.difficulty, easy)
-                    res.body.should.have.deep.property("question.title")
-                    assert.equal(res.body.question.title, "test")
-                    res.body.should.have.deep.property("question.description")
-                    assert.equal(res.body.question.description, "test")
-                    res.body.should.have.deep.property("question.link")
-                    assert.equal(res.body.question.link, "test")
-                    done()
+                    res.body.should.have.property("message")
+                    assert.equal(res.body.message, `interview history saved for ${userHistory.username}`)
                 })
         })
     })
@@ -41,7 +24,7 @@ describe("history-service tests", () => {
     describe("GET/find-history/:username", () => {
         it("Get history succeessful", (done) => {
             chai.request(app)
-            .get(`/find-history/${username}`)
+            .get(`/find-history/${userHistory.username}`)
             .end((err, res) => {
                 res.should.have.status(200)
                 res.body.should.be.a("object")
