900b2c9f680ecf23ef680415da980b6f1e66e1bc
Herrekt <herrickkohyukan@gmail.com>
2022-10-13 03:11:59 +0800

    Changed to python
    Changed and refactored code editor to python

--- frontend/src/components/CodeEditor.js
+++ frontend/src/components/CodeEditor.js
@@ -1,33 +1,34 @@
 import { useState } from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { javascript } from "@codemirror/lang-javascript";
+//import { javascript } from "@codemirror/lang-javascript";
+import { python } from "@codemirror/lang-python";
 import { sublime } from "@uiw/codemirror-theme-sublime";
 
 const CodeEditor = ({ roomId, socket }) => {
-  const [code, setCode] = useState("");
+    const [code, setCode] = useState("");
 
-  socket.on("RECEIVE", (payload) => {
-    console.log("Received" + payload.code);
-    setCode(payload.code);
-  });
+    socket.on("RECEIVE", (payload) => {
+        console.log("Received" + payload.code);
+        setCode(payload.code);
+    });
 
-  function updateCode(roomId, code) {
-    console.log("Change happening", code);
-    setCode(code);
-    socket.emit("CHANGE", { roomId: roomId, code: code });
-  }
+    function updateCode(roomId, code) {
+        console.log("Change happening", code);
+        setCode(code);
+        socket.emit("CHANGE", { roomId: roomId, code: code });
+    }
 
-  return (
-    <div>
-      <CodeMirror
-        className="container"
-        value={code}
-        height="50em"
-        onChange={(code) => updateCode(roomId, code)}
-        extensions={[javascript({ jsx: true })]}
-        theme={sublime}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <CodeMirror
+                className="container"
+                value={code}
+                height="50em"
+                onChange={(code) => updateCode(roomId, code)}
+                extensions={[python()]}
+                theme={sublime}
+            />
+        </div>
+    );
 };
 export default CodeEditor;
