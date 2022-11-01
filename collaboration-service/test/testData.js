71088f280d9155be2263aec273eb65769fb11526
Herrekt <herrickkohyukan@gmail.com>
2022-11-01 20:22:43 +0800

    Revert "Refactoring semicolons and quotation"
    This reverts commit fbabbfd72c4a4b914a8c45164b5dd41a15ae2769.

--- collaboration-service/test/testData.js
+++ collaboration-service/test/testData.js
@@ -1,10 +1,10 @@
 export const roomId = {
-    'roomId': '123456789'
+    "roomId": "123456789"
 }
 
 export const codeChange = {
-    'roomId': '123456789',
-    'code': "print('Hello World')"
+    "roomId": "123456789",
+    "code": "print('Hello World')"
 }
 
-export const localUrl = 'http://localhost:8003'
+export const localUrl = "http://localhost:8003";
