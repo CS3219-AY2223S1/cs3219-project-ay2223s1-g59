657d947e0010025948f7d6a89f405d71b3df0e4a
ongweisheng <ongweisheng30@gmail.com>
2022-11-05 21:53:16 +0800

    Update createHistory api to check unique

--- history-service/controllers/historyController.js
+++ history-service/controllers/historyController.js
@@ -2,8 +2,13 @@ import History from "../models/history.js"
 
 export const createHistory = async (req, res) => {
     try {
-        const history = await History.create(req.body)
-        return res.status(200).json({ message: `interview history saved for ${history.username}` })
+        const history = await History.findOne(req.body)
+        if (history) {
+            return res.status(200).json({ message: `history already created previously for ${history.username}` })
+        } else {
+            history = await History.create(req.body)
+            return res.status(200).json({ message: `interview history saved for ${history.username}` })
+        }
     } catch (err) {
         return res.status(500).json(err)
     }
