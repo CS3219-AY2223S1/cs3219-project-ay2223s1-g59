620fce30bb5671804f165efd9536afc294989fcb
hiongkaihan <kaihanhiong@gmail.com>
2022-11-08 01:10:41 +0800

    Update test.js

--- chat-service/test/test.js
+++ chat-service/test/test.js
@@ -7,12 +7,12 @@ let socketUrl = 'http://localhost:8005' // Will abstract to constants file durin
 chai.should()
 
 describe('One user joins room', () => {  
-    it('should return user joined room', done => {    
+    it('should return user joined interview', done => {    
         const client = io(socketUrl)
 
         client.emit('join', user1)
         client.on('receive', (res) => {
-            res.message.should.equal('CS2101 has joined the chat room')
+            res.message.should.equal('CS2101 has joined the interview')
             res.username.should.equal('ChatBot')
             client.removeListener('receive')
             done()
@@ -21,15 +21,15 @@ describe('One user joins room', () => {
 })
 
 describe('Two users join room', () => {  
-    it('should return second user joined room', done => {
+    it('should return second user joined interview', done => {
         const client1 = io(socketUrl)
         const client2 = io(socketUrl)
 
         client1.emit('join', user1)
         client2.emit('join', user2)
         client1.on('receive', (res) => {
-            if (res.message === "CS2102 has joined the chat room") {
-                res.message.should.equal('CS2102 has joined the chat room')
+            if (res.message === "CS2102 has joined the interview") {
+                res.message.should.equal('CS2102 has joined the interview')
                 res.username.should.equal('ChatBot')
                 client1.removeListener('receive')
                 done()
